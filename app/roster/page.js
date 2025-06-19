c8b8a0a7645709cb5c1c942b513bbe7bf0a4c0e4
Teran <teranarthur99@gmail.com>
2025-06-18 18:29:28 -0700

    last commit

--- app/roster/page.js
+++ app/roster/page.js
@@ -1,9 +1,20 @@
+'use client';
+import { useState } from "react";
+import { PlayerAPI } from "../data/playerAPI";
+import Link from "next/link";
 
 export default function Roster() {
+  const [allPlayers, _] = useState(PlayerAPI.all());
+
   return (
     <main>
       <div>
         <h1>You are now viewing the Roster page.</h1>
+        {allPlayers.map((p) => (
+          <li key={p.number}>
+            <Link href={`/roster/${p.number}`}>{p.name}</Link>
+          </li>
+        ))}
       </div>
     </main>
   );
